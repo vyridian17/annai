e9423f3cdfcf16047136605f5c78453debf104f2
rituraj8503 <rituraj8@uw.edu>
2021-08-03 17:40:12 +0900

    worked on the front end a bit

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,28 @@
 <template>
   <div id="app">
     <div id="nav">
+    <Navbar />
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
   </div>
 </template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+
+export default {
+  name:"App",
+  components: {
+    Navbar
+  }
+}
+</script>
+
+
+
+
 
 <style>
 #app {
