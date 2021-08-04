f6f9c9fba48b5a36567b033676e60a34091bfb63
rituraj8503 <rituraj8@uw.edu>
2021-08-04 21:08:34 +0900

    Done

--- client/src/views/Checkout.vue
+++ client/src/views/Checkout.vue
@@ -24,13 +24,14 @@ export default {
     StripeCheckout,
   },
    data () {
-    this.guide = this.$store.state.selectedGuide;
     this.publishableKey = "pk_test_51JKcaVJEzx4OWsdKM7OUINRiAb2jvkNEDvkviGKded4631cm456GNj5usis6ZJ2Q07tAe20OautxgOQVv08sbmlt00jiAEFmH4";
+    var price_id = String(this.$store.state.selectedGuide.price_id);
+    // console.log(price_id);
     return {
      loading: false,
       lineItems: [
         {
-          price: this.$store.state.selectedGuide.price_id, // this.$store.state.selectedGuide.price_id  The id of the one-time price you created in your Stripe dashboard
+          price: price_id, // this.$store.state.selectedGuide.price_id 
           quantity: 1
           // images: this.$store.state.selectedGuide.img
         },
--- server/server.js
+++ server/server.js
@@ -66,25 +66,25 @@ app.post("/guides/new", async (req, res) => {
     }
 })
 
-// app.post('/create-checkout-session', cors(), async (req, res) => {
-//     const session = await stripe.checkout.sessions.create({
-//       payment_method_types: [
-//         'card',
-//       ],
-//       line_items: [
-//         {
-//           // TODO: replace this with the `price` of the product you want to sell
-//           price: '{{PRICE_ID}}',
-//           quantity: 1,
-//         },
-//       ],
-//       mode: 'payment',
-//       success_url: `${YOUR_DOMAIN}/success.html`,
-//       cancel_url: `${YOUR_DOMAIN}/cancel.html`,
-//     });
-//     res.redirect(303, session.url)
-//   });
-// /*
+app.post('/create-checkout-session', cors(), async (req, res) => {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: [
+        'card',
+      ],
+      line_items: [
+        {
+          // TODO: replace this with the `price` of the product you want to sell
+          price: '{{PRICE_ID}}',
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: `${YOUR_DOMAIN}/success.html`,
+      cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+    });
+    res.redirect(303, session.url)
+  });
+/*
 
 // {id: 1, first_name: 'Bob', last_name: "Marley", email: "ishotthesherrif@deputy.com", tour_locations: "Tokyo", languages: "English", hourly_rate: 6000, info: " "},
 
