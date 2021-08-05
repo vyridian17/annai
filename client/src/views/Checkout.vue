02c88f79d132729fd2f1808b9504d06242a88d8b
rituraj8503 <rituraj8@uw.edu>
2021-08-05 10:59:36 +0900

    Pushed

--- client/src/views/Checkout.vue
+++ client/src/views/Checkout.vue
@@ -25,13 +25,12 @@ export default {
   },
    data () {
     this.publishableKey = "pk_test_51JKcaVJEzx4OWsdKM7OUINRiAb2jvkNEDvkviGKded4631cm456GNj5usis6ZJ2Q07tAe20OautxgOQVv08sbmlt00jiAEFmH4";
-    var price_id = String(this.$store.state.selectedGuide.price_id);
     // console.log(price_id);
     return {
      loading: false,
       lineItems: [
         {
-          price: price_id, // this.$store.state.selectedGuide.price_id 
+          price: this.$store.state.selectedGuide.price_id, // this.$store.state.selectedGuide.price_id 
           quantity: 1
           // images: this.$store.state.selectedGuide.img
         },
